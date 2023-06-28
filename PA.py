dbcc77bce0ffe5a431b2a780bc8d379f2c0d0f03
Gian_Chavez/019 <105017261+DGian019@users.noreply.github.com>
2023-06-28 13:56:57 -0500

    Relleno de Datos para las 3 Columnas P-Q-R

--- PA.py
+++ PA.py
@@ -16,6 +16,7 @@ proyectos = df.iloc[3:, 6].dropna().unique().tolist()
 # Obtener los códigos de componente únicos
 codigos_componente = df.iloc[3:, 8].dropna().unique().tolist()
 
+
 # Ruta del nuevo archivo Excel (PA'S)
 nuevo_archivo_excel = r"\\RIDER\DocInterno\PEI - PDI 2022 - 2031\PLANES DE ACCIÓN 2023\12. Dir. Rec Físicos e Infr\12. Dir. Rec Físicos e Infr.xlsm"
 nueva_hoja = "2. Seguimiento"
@@ -24,11 +25,14 @@ nueva_hoja = "2. Seguimiento"
 df_nuevo = pd.read_excel(nuevo_archivo_excel, sheet_name=nueva_hoja)
 
 # Obtener las columnas necesarias del nuevo archivo
-columna1_nuevo = df_nuevo.iloc[:, 15].unique()  # Columna "P" (índice 15)
+columna1_nuevo = df_nuevo.iloc[:, 15]  # Columna "P" (índice 15)
 columna2_nuevo = df_nuevo.iloc[:, 16].unique()  # Columna "Q" (índice 16)
-columna3_nuevo = df_nuevo.iloc[:, 17].unique()  # Columna "R" (índice 17)
+columna3_nuevo = df_nuevo.iloc[:, 17]  # Columna "R" (índice 17)
 
+
+# Columna de Proyecto para comprarla con POAI2023
 proyectos_nuevo = df_nuevo.iloc[:, 5].dropna().unique().tolist()
+print(df_nuevo.head())
 
 def mostrar_datos(event):
     # Obtener el valor seleccionado de la lista desplegable (proyecto seleccionado)
@@ -56,10 +60,12 @@ def mostrar_datos(event):
         if proyecto_seleccionado in proyectos_nuevo:
             # Filtrar el DataFrame nuevo por el proyecto seleccionado en la columna "F" (POAI2023)
             datos_nuevo_filtrados = df_nuevo[df_nuevo.iloc[:, 5] == proyecto_seleccionado]
-
+            
             for _, nuevo_row in datos_nuevo_filtrados.iterrows():
-                valores_nuevo = [nuevo_row[i] for i in nombres_columnas_nuevo]
-
+                valores_nuevo = [nuevo_row.iloc[15] if pd.notnull(nuevo_row.iloc[15]) else None, # Columna "P" (índice 15)
+                                nuevo_row.iloc[16] if pd.notnull(nuevo_row.iloc[16]) else None,  # Columna "Q" (índice 16)
+                                nuevo_row.iloc[17] if pd.notnull(nuevo_row.iloc[17]) else None   # Columna "R" (índice 17)
+                                ]
                 # Agregar los valores de las columnas seleccionadas del archivo nuevo
                 valores.extend(valores_nuevo)
 
@@ -95,7 +101,7 @@ treeview = ttk.Treeview(window)
 nombres_columnas_origen = ["PERSPECTIVA DEL BSC", "EJE", "OBJETIVO ESTRATÉGICO", "POLITICA", "PROGRAMA ", "RESPONSABLE DE PROGRAMA", "PROYECTO", "RESPONSABLE DE PROYECTO", "CÓDIGO COMPONENTE", "COMPONENTES", "COMPONENTES Concatenados", "INDICADOR", "META", "MEGAS", "MACROACTIVIDADES", "ACTIVIDAD", "RESPONSABLE COMPONENTE"]
 
 # Definir los nombres de las columnas del archivo nuevo
-nombres_columnas_nuevo = ["PERIODO DE EJECUCIÓN", "% AVANCE EN EL APORTE DE LA ACTIVIDAD AL COMPONENTE EN EL AÑO", "% DE CUMPLIMIENTO DEL INDICADOR DE GESTIÓN"]
+nombres_columnas_nuevo = ["PERIODO DE EJECUCIÓN","% AVANCE EN EL APORTE DE LA ACTIVIDAD AL COMPONENTE EN EL AÑO","% DE CUMPLIMIENTO DEL INDICADOR DE GESTIÓN"]
 
 # Crear un diccionario que mapee los índices de columna a sus respectivos nombres
 columnas = {i: nombre for i, nombre in enumerate(nombres_columnas_origen + nombres_columnas_nuevo)}
@@ -104,10 +110,10 @@ columnas = {i: nombre for i, nombre in enumerate(nombres_columnas_origen + nombr
 treeview["columns"] = tuple(columnas.keys())
 treeview.heading("#0", text="Índice")
 
+
 # Configurar los nombres de las columnas
 for indice, nombre_columna in columnas.items():
     treeview.heading(str(indice), text=f"{indice}: {nombre_columna}")
-
 
 # Mostrar el Treeview vacío inicialmente
 treeview.pack(fill="both", expand=True)
